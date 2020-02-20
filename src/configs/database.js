971f8974569dbde89535b7aa7c462acb32a3db50
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-20 20:39:53 -0300

    Adicionando arquivos

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,11 +1,36 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'admin',
-  database: 'dbadministrai',
-  define: {
-    timestamps: true,
-    freezeTableName: true
+  "development": {
+      dialect: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: 'admin',
+      database: 'dbadministrai',
+      define: {
+          timestamps: true,
+          freezeTableName: true
+      },
   },
+  "staging": {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'admin',
+    database: 'dbadministrai',
+    define: {
+        timestamps: true,
+        freezeTableName: true
+    },
+  },
+  "production": {
+      "use_env_variable": "DATABASE_URL",
+      dialect:  'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true,
+      database: 'dbadministrai',
+      define: {
+          timestamps: true,
+          freezeTableName: true
+      }
+  }
 };
