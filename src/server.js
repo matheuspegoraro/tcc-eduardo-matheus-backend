668c8517b505a03cf77eb3cbe07853b4dd8967c2
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-21 16:10:02 -0300

    Adicionando env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 const server = require('http').createServer(app);
