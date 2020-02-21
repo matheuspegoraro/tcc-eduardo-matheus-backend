03bc0fd3c8aa578e8bff182ae432518fb5039983
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-21 15:14:39 -0300

    Alterando conexão com banco

--- src/configs/database.js
+++ src/configs/database.js
@@ -24,8 +24,6 @@ module.exports = {
   "production": {
       "use_env_variable": "DATABASE_URL",
       dialect:  'postgres',
-      port:     match[4],
-      host:     match[3],
       logging:  true,
       database: 'dbadministrai',
       define: {
