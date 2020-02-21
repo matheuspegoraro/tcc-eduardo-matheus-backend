0d4008a3a0b0f69164454666413e41de7e10eec1
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-21 18:21:57 -0300

    Alterando o database.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,34 +1,10 @@
 module.exports = {
-  "development": {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'admin',
-    database: 'dbadministrai',
-    define: {
-        timestamps: true,
-        freezeTableName: true
-    },
+  dialect:  'postgres',
+  use_env_variable: "DATABASE_URL",
+  logging:  true,
+  database: 'dbadministrai',
+  define: {
+      timestamps: true,
+      freezeTableName: true
   },
-  "staging": {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'admin',
-    database: 'dbadministrai',
-    define: {
-        timestamps: true,
-        freezeTableName: true
-    },
-  },
-  "production": {
-    dialect:  'postgres',
-    use_env_variable: "DATABASE_URL",
-    logging:  true,
-    database: 'dbadministrai',
-    define: {
-        timestamps: true,
-        freezeTableName: true
-    },
-  }
 };
