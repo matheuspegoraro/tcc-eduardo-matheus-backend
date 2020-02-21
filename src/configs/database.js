9759fe72a2b7f3435e3484d3569180768b1ef976
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-21 18:11:18 -0300

    Corrigindo arquivos

--- src/configs/database.js
+++ src/configs/database.js
@@ -22,18 +22,6 @@ module.exports = {
     },
   },
   "production": {
-<<<<<<< HEAD
-      "use_env_variable": "DATABASE_URL",
-      dialect: 'postgres',
-      port: match[4],
-      host: match[3],
-      logging: true,
-      database: 'dbadministrai',
-      define: {
-          timestamps: true,
-          freezeTableName: true
-      }
-=======
     dialect:  'postgres',
     use_env_variable: "DATABASE_URL",
     logging:  true,
@@ -41,7 +29,6 @@ module.exports = {
     define: {
         timestamps: true,
         freezeTableName: true
-    }
->>>>>>> 9f5046d2f1c197e475f3d189e15c6c6cb159b7a3
+    },
   }
 };
