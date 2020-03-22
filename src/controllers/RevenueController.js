faa6765fb82f54d9ebd81370b6a7382f7c0c5926
Eduardo Geraldini <eduardopr.geraldini@gmail.com>
2020-03-22 19:43:40 -0300

    desenvolvimento do modulo de receitas

---
+++ src/controllers/RevenueController.js
@@ -0,0 +1,332 @@
+const Movement = require("../models/Movement");
+const Company = require("../models/Company");
+const Bill = require("../models/Bill");
+const MovementType = require("../models/MovementType");
+const Category = require("../models/Category");
+const httpStatus = require("http-status");
+
+//MOVEMENTS TYPES:
+//1 - PAGAMENTOS;
+//2 - RECEBIMENTOS;
+//3 - TRANSFERÊNCIAS;
+
+const REVENUES = 2;
+
+module.exports = {
+  async list(req, res) {
+    const { companyId } = req;
+
+    try {
+      const movements = await Movement.findAll({
+        attributes: [
+          "id",
+          "name",
+          "value",
+          "date",
+          "done",
+          "dischargeDate",
+          "createdAt",
+          "updatedAt"
+        ],
+        include: [
+          {
+            model: Company,
+            as: "company",
+            attributes: ["id", "name"]
+          },
+          {
+            model: Bill,
+            as: "bill",
+            attributes: ["id", "name", "currentValue"]
+          },
+          {
+            model: MovementType,
+            as: "movementType",
+            attributes: ["id", "name"]
+          },
+          {
+            model: Category,
+            as: "category",
+            attributes: ["id", "name", "color"]
+          }
+        ],
+        where: {
+          companyId,
+          movementTypeId: REVENUES
+        },
+        order: [["date", "DESC"]]
+      });
+
+      return res.status(httpStatus.OK).json(movements);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error: "Problema ao listar as receitas! Por favor, tente mais tarde."
+      });
+    }
+  },
+
+  async create(req, res) {
+    const { companyId } = req;
+    const {
+      billId,
+      movementTypeId,
+      categoryId,
+      name,
+      value,
+      date,
+      done,
+      dischargeDate
+    } = req.body;
+
+    try {
+      if (done) {
+        if (!dischargeDate)
+          return res.status(httpStatus.BAD_REQUEST).json({
+            error: "Data do recebimento é obrigatório!"
+          });
+
+        Bill.increment({ currentValue: value }, { where: { id: billId } });
+      }
+
+      const movement = await Movement.create({
+        companyId,
+        billId,
+        movementTypeId,
+        categoryId,
+        name,
+        value,
+        date,
+        done,
+        dischargeDate
+      });
+
+      return res.status(httpStatus.OK).json(movement);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error: "Problemas ao criar a receita! Por favor, tente mais tarde."
+      });
+    }
+  },
+
+  async update(req, res) {
+    const { companyId } = req;
+    const { movementId } = req.params;
+    const {
+      billId,
+      movementTypeId,
+      categoryId,
+      name,
+      value,
+      date,
+      done
+    } = req.body;
+
+    try {
+      const movement = await Movement.findByPk(movementId);
+
+      if (!movement)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não encontramos o movimento informado!" });
+
+      if (movement.done)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não é permitido editar recebimentos já realizados!" });
+
+      movement.update({
+        companyId,
+        billId,
+        movementTypeId,
+        categoryId,
+        name,
+        value,
+        date,
+        done
+      });
+
+      return res.status(httpStatus.OK).json(movement);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error: "Problemas ao atualizar o movimento! Por favor, tente mais tarde."
+      });
+    }
+  },
+
+  async byId(req, res) {
+    const { movementId } = req.params;
+    const { companyId } = req;
+
+    try {
+      const movement = await Movement.findOne({
+        include: [
+          {
+            model: Company,
+            as: "company",
+            attributes: ["id", "name"]
+          },
+          {
+            model: Bill,
+            as: "bill",
+            attributes: ["id", "name", "currentValue"]
+          },
+          {
+            model: MovementType,
+            as: "movementType",
+            attributes: ["id", "name", "icon"]
+          },
+          {
+            model: Category,
+            as: "category",
+            attributes: ["id", "name", "color"]
+          }
+        ],
+        where: {
+          id: movementId,
+          companyId
+        }
+      });
+
+      if (!movement)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não encontramos o movimento informado!" });
+
+      return res.json(movement);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error: "Ocorreu um erro na requisição! Por favor, tente mais tarde."
+      });
+    }
+  },
+
+  async delete(req, res) {
+    const { movementId } = req.params;
+    const { companyId } = req;
+
+    try {
+      const movement = await Movement.findOne({
+        where: {
+          id: movementId,
+          companyId
+        }
+      });
+
+      if (!movement)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não encontramos as informações do movimento!" });
+
+      if (movement.done) {
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não é permitido deletar recebimentos já realizados!" });
+      }
+
+      await movement.destroy();
+      return res.status(httpStatus.OK).json(movement);
+    } catch (error) {
+      return res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ error: "Não foi possível deletar o movimento!" });
+    }
+  },
+
+  async makeReceipt(req, res) {
+    const { companyId } = req;
+    const { movementId, dischargeDate, value } = req.body;
+    let diferenceOfValue;
+
+    try {
+      const movement = await Movement.findOne({
+        where: {
+          id: movementId,
+          companyId
+        }
+      });
+
+      if (!movement)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não encontramos o movimento informado!" });
+
+      if (movement.value < value)
+        return res.status(httpStatus.BAD_REQUEST).json({
+          error: "O valor de recebimento deve ser menor ou igual ao valor do movimento!"
+        });
+
+      if (movement.value != value) {
+        diferenceOfValue = movement.value - value;
+
+        movement.update({
+          dischargeDate,
+          value,
+          done: true
+        });
+
+        Movement.create({
+          companyId,
+          billId: movement.billId,
+          movementTypeId: movement.movementTypeId,
+          categoryId: movement.categoryId,
+          name: movement.name + " [ACERTO DE DIFERENÇA]",
+          value: diferenceOfValue,
+          date: movement.date
+        });
+      } else {
+        movement.update({
+          dischargeDate,
+          done: true
+        });
+      }
+
+      Bill.increment(
+        { currentValue: value },
+        { where: { id: movement.billId } }
+      );
+
+      return res.status(httpStatus.OK).json(movement);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error:
+          "Encontramos problemas ao realizar a baixa! Por favor, tente mais tarde."
+      });
+    }
+  },
+
+  async undoReceipt(req, res) {
+    const { companyId } = req;
+    const { movementId } = req.params;
+
+    try {
+      const movement = await Movement.findOne({
+        where: {
+          id: movementId,
+          companyId
+        }
+      });
+
+      if (!movement)
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ error: "Não encontramos as informações do movimento!" });
+
+      movement.update({
+        dischargeDate: null,
+        done: false
+      });
+
+      Bill.decrement(
+        { currentValue: movement.value },
+        { where: { id: movement.billId } }
+      );
+
+      return res.status(httpStatus.OK).json(movement);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        error:
+          "Encontramos problemas ao desfazer o movimento! Por favor, tente mais tarde."
+      });
+    }
+  }
+};
--- src/routes/movement.js
+++ src/routes/movement.js
@@ -1,16 +1,29 @@
 const express = require('express');
 
 const ExpenseController = require('../controllers/ExpenseController');
+const RevenueController = require('../controllers/RevenueController');
 const AuthMiddleware = require('../middlewares/auth');
 
 const movement = express.Router();
 
-movement.get('/movements', AuthMiddleware, ExpenseController.list);
-movement.get('/movements/:movementId', AuthMiddleware, ExpenseController.byId);
-movement.post('/movements', AuthMiddleware, ExpenseController.create);
-movement.put('/movements/:movementId', AuthMiddleware, ExpenseController.update);
-movement.delete('/movements/:movementId', AuthMiddleware, ExpenseController.delete);
-movement.post('/movements/make-payment', AuthMiddleware, ExpenseController.makePayment);
-movement.put('/movements/undo-payment/:movementId', AuthMiddleware, ExpenseController.undoPayment);
+//EXPENSES
+movement.get('/movements/expenses', AuthMiddleware, ExpenseController.list);
+movement.get('/movements/expenses/:movementId', AuthMiddleware, ExpenseController.byId);
+movement.post('/movements/expenses', AuthMiddleware, ExpenseController.create);
+movement.put('/movements/expenses/:movementId', AuthMiddleware, ExpenseController.update);
+movement.delete('/movements/expenses/:movementId', AuthMiddleware, ExpenseController.delete);
+movement.post('/movements/expenses/make-payment', AuthMiddleware, ExpenseController.makePayment);
+movement.put('/movements/expenses/undo-payment/:movementId', AuthMiddleware, ExpenseController.undoPayment);
+
+//REVENUES
+movement.get('/movements/revenues', AuthMiddleware, RevenueController.list);
+movement.get('/movements/revenues/:movementId', AuthMiddleware, RevenueController.byId);
+movement.post('/movements/revenues', AuthMiddleware, RevenueController.create);
+movement.put('/movements/revenues/:movementId', AuthMiddleware, RevenueController.update);
+movement.delete('/movements/revenues/:movementId', AuthMiddleware, RevenueController.delete);
+movement.post('/movements/revenues/make-receipt', AuthMiddleware, RevenueController.makeReceipt);
+movement.put('/movements/revenues/undo-receipt/:movementId', AuthMiddleware, RevenueController.undoReceipt);
+
+//TRANSFERS
 
 module.exports = movement;
