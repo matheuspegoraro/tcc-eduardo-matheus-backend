02317abadb14874bb1868b31e3c0ba662cc0850d
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-04-19 02:06:46 -0300

    Finalizando relacionamento entre clientes e consultorias

--- src/controllers/RelationshipCompanyController.js
+++ src/controllers/RelationshipCompanyController.js
@@ -3,57 +3,63 @@ const RelationshipCompany = require('../models/RelationshipCompany');
 const httpStatus = require('http-status');  
 
 module.exports = {
-  async create(req, res) {
-    const { advisoryId, selectedClients } = req.body;
-
-    const relationships = [];
-
-    try {
-      const destroiedRelationships = RelationshipCompany.destroy({
-        where: { 'advisoryId': advisoryId }
-      });
-
-      selectedClients.map(async clientId => {        
-        const relationshipFind = await RelationshipCompany.findOne({ where: { clientId, advisoryId } });
-
-        if (relationshipFind){
-          return res.status(httpStatus.BAD_REQUEST).json({ error: `Relacionamento entre o cliente ${clientId} e a consuloria ${advisoryId}` });
-        }
-
-        relationships.push(await RelationshipCompany.create({ clientId, advisoryId }));
-      });
-
-      return res.status(httpStatus.OK).json(relationships);
-    } catch (error) {
-      console.log(error);
-      return res.status(httpStatus.BAD_REQUEST).json({ error: 'Problems requesting route!' });
-    }
-  }, 
-
-  async byId(req, res) {
-    const { advisoryId } = req.params;
+  // Pegará todos as consultorias que estão relacionadas ao cliente
+  async byClientId(req, res) {
+    const { clientId } = req.params;
 
     try {
-      const clients = await RelationshipCompany.findAll({
-        attributes: ['clientId'],
+      const advisories = await RelationshipCompany.findAll({
         include: [{
-          model: Company,
-          as: 'clients',
-          attributes: ['id', 'name', 'type']
-        }, {
           model: Company,
           as: 'advisories',
           attributes: ['id', 'name', 'type']
         }],
         where: {
-          advisoryId
+          clientId
         }
       });
 
-      return res.status(httpStatus.OK).json(clients);
+      return res.status(httpStatus.OK).json(advisories);
     } catch (error) {
       console.log(error);
       return res.status(httpStatus.BAD_REQUEST).json({ error: 'Problemas ao requisitar relacionamento de clientes!' });
     }
   }, 
+
+  async create(req, res) {
+    const { clientId, advisoryId } = req.body;
+
+    try {
+      const relationshipFind = await RelationshipCompany.findOne({ where: { clientId, advisoryId } });
+
+      if (relationshipFind){
+        return res.status(httpStatus.BAD_REQUEST).json({ error: `Relacionamento entre o cliente ${clientId} e a consuloria ${advisoryId} já existe` });
+      }
+
+      const relationship = await RelationshipCompany.create({ clientId, advisoryId });
+
+      return res.status(httpStatus.OK).json(relationship);
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({ error: 'Problems requesting route!' });
+    }
+  }, 
+
+  async delete(req, res) {
+    const { relationshipId } = req.params;
+
+    try {
+      const relationship = await RelationshipCompany.findByPk(relationshipId);
+
+      if (relationship) {
+        relationship.destroy()
+          .then(() => {
+            res.status(httpStatus.OK).json(relationship);
+          })
+      } else {
+        return res.status(httpStatus.BAD_REQUEST).json({ error: 'Erro ao tentar remover a relação entre Cliente e Consultoria! Categoria não existente na base de dados.' });
+      }
+    } catch (error) {
+      return res.status(httpStatus.BAD_REQUEST).json({ error: 'Erro ao tentar remover a relação entre Cliente e Consultoria! Por favor, tente mais tarde.' });
+    }
+  },
 };
--- src/routes/relationshipCompany.js
+++ src/routes/relationshipCompany.js
@@ -5,7 +5,8 @@ const AuthMiddleware = require('../middlewares/auth');
 
 const company = express.Router();
 
-company.get('/relationship-company/:advisoryId', AuthMiddleware, RelationshipCompanyController.byId);
+company.get('/relationship-company/:clientId', AuthMiddleware, RelationshipCompanyController.byClientId);
 company.post('/relationship-company', AuthMiddleware, RelationshipCompanyController.create);
+company.delete('/relationship-company/:relationshipId', AuthMiddleware, RelationshipCompanyController.delete);
 
 module.exports = company;
